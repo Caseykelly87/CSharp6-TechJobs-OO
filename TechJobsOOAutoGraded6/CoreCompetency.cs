7834692afdcea8efa4014928f9c6b0f6cffbe23a
Casey Kelly <Caseykelly87@gmail.com>
2024-06-07 11:04:43 -0500

    Assignment Complete

--- TechJobsOOAutoGraded6/CoreCompetency.cs
+++ TechJobsOOAutoGraded6/CoreCompetency.cs
@@ -4,7 +4,6 @@ namespace TechJobsOOAutoGraded6
 	public class CoreCompetency : JobField
 	{
 
-        // TODO: Task 2: Change the fields to auto-implemented properties.
 
         public CoreCompetency(string value) : base (value)
         {
